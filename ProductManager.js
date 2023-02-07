7a9a0484a736ff0e76fc9a5ea3fc916907560109
David Bareiro <davidbareiro@gmail.com>
2023-02-07 18:42:56 -0300

    desafio modificado

--- ProductManager.js
+++ ProductManager.js
@@ -1,41 +1,58 @@
-import { promises as fs } from "fs";
 
-class MisProductos  {
+import { promises as fs } from "fs";
+class Producto {
+  constructor(titulo, descripcion, precio, img, code, stock) {
+      this.titulo = titulo;
+      this.descripcion = descripcion;
+      this.precio = precio;
+      this.img = img;
+      this.code = code;
+      this.stock = stock;
+  }
+}
+
+class ProductManager  {
   constructor(path) {
     this.path = path;
   }
- async addProduct(object) {
-    const MiCode = this.Productos.map((p) => p.code);
-    const Revision = MiCode.includes(object.code);
-    if (Revision) {
-      console.log("Este codigo ya figura en la base de datos, por favor verifique");
-    } else if (Object.values(object).includes("")) {
-      console.log( "Complete los campos, no pueden estar vacio");
-    } 
-    else 
-    {
-      let id;
-      id = this.Productos.length + 1;
-      const NuevoProducto = { ...object, id };
-      this.Productos.push(NuevoProducto);
-      return console.log(` Se agrego a la base un nuevo porducto      : ${NuevoProducto.id} `);
-    }
-  }
-
-
-  async getProducts() {
+  async addProduct(ProdNuevo) {
     try {
-      const NuevoValor = await fs.readFile(this.path, "utf8");
-      return JSON.parse(NuevoValor);
+      const read = await fs.readFile(this.path, "utf8");
+      const data = JSON.parse(read);
+      const objCode = data.map((product) => product.code);
+      const BuscoCodigo = objCode.includes(ProdNuevo.code);
+      if (BuscoCodigo) {
+        console.log("Codigo de producto existente, intente otro");
+      } else if (Object.values(ProdNuevo).includes("")) {
+        console.log("Complete los campos, no pueden estar vacio" );
+      } 
+      else {
+        let id;
+        data.length === 0 ? (id = 1) : (id = data[data.length - 1].id + 1)
+        const NuevoArr = { ...ProdNuevo, id };
+        data.push(NuevoArr);
+        await fs.writeFile(this.path, JSON.stringify(data, null, 2), "utf-8");
+        return console.log(`Se agrego a la base un nuevo porducto con id: ${NuevoArr.id} `  );
+      }
     } catch (error) {
       throw error;
     }
   }
-
-  async getPorductById(id) {
+ 
+  async getProducts() {
     try {
-      const NuevoValor = await fs.readFile(this.path, "utf-8");
-      const data = JSON.parse(NuevoValor);
+      const read = await fs.readFile(this.path, "utf8");
+      return JSON.parse(read);
+    } catch (error) {
+      throw error;
+    }
+  }
+ 
+  
+  async getById(id) {
+    try {
+      const read = await fs.readFile(this.path, "utf-8");
+      const data = JSON.parse(read);
       const product = data.find((product) => product.id === id);
       if (product) {
         return product;
@@ -47,37 +64,57 @@ class MisProductos  {
     }
   }
 
+  
+
   async deleteProduct(id) {
     try {
-      const NuevoValor = await fs.readFile(this.path, "utf-8");
-      const InfoJs = JSON.parse(NuevoValor);
-      const BorrarItem = JSON.stringify(
-        InfoJs.find((product) => product.id === id)
+      const read = await fs.readFile(this.path, "utf-8");
+      const data = JSON.parse(read);
+      const Deletear = JSON.stringify(
+        data.find((product) => product.id === id)
       );
-      const nuevaINfo = InfoJs.filter((product) => product.id !== id);
-      await fs.writeFile(this.path, JSON.stringify(nuevaINfo), "utf-8");
-      return console.log(
-        `El producto ${BorrarItem} se elimino`
-      );
+      const NuevoArr = data.filter((product) => product.id !== id);
+      await fs.writeFile(this.path, JSON.stringify(NuevoArr), "utf-8");
+      return console.log(`El producto ${Deletear} ha sido eliminado  `);
     } catch (error) {
       throw error;
     }
   }
+ 
+
   async updateProduct(id, entry, value) {
     try {
-      const NuevoValor = await fs.readFile(this.path, "utf-8");
-      const InfoJs = JSON.parse(NuevoValor);
-      const index = InfoJs.findIndex((product) => product.id === id);
-      if(!InfoJs[index][entry]){
+      const read = await fs.readFile(this.path, "utf-8");
+      const data = JSON.parse(read);
+      const index = data.findIndex((product) => product.id === id);
+      if(!data[index][entry]){
         throw Error
       }
-      InfoJs[index][entry] = value;
-      await fs.writeFile(this.path, JSON.stringify(InfoJs, null, 2));
-      return console.log(InfoJs); 
+      data[index][entry] = value;
+      await fs.writeFile(this.path, JSON.stringify(data, null, 2));
+      return console.log(data); 
     } catch (error) {
       console.log('Not found');
     }
   }
+
 }
-const products = new MisProductos ("./data.json");
+
+
+const Prod = new Producto("David", "Bareiro", 120, "www.ole.com.ar", "150", 10);
+const productManager = new ProductManager("./data.json");
+
+const Probando = async()=>{
+  await productManager.addProduct(Prod)
+    
+  console.log(await productManager.getProducts());
+  // console.log(await productManager.getById(2));
+  // console.log(await productManager.updateProduct(2, "precio", 210))
+  //  console.log(await productManager.getById(1))
+  // console.log(await productManager.deleteProduct(1));
+
+}
+Probando();
+
+export default ProductManager;
 
